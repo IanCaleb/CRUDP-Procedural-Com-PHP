309a5c5aa64d324d9c0e25bfa2a0d762546c6966
IanCaleb <ivusaw1227@gmail.com>
2025-11-07 11:33:24 -0300

    componente de mensagem de feedback do crud

---
+++ mensagem.php
@@ -0,0 +1,16 @@
+<?php 
+    if(isset($_SESSION['mensagem'])){
+?>
+
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <?=$_SESSION['mensagem'];?>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+    <!---->
+</div>
+
+<?php 
+    //destroi a variavel referenciada;
+    unset($_SESSION['mensagem']);
+    }
+?>
